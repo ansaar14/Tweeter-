de60ef24497178d7a0bdf040626b3ca26a191be2
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-17 22:52:23 +0000

    function that count characters in textarea

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+  $('textarea').keyup( function() {
+    // console.log(this);
+
+    let value = $(this).val();
+    let maxlength = $(this).data("maxlength");
+    
+    let total = maxlength - value.length;
+    console.log(total);
+
+    if (total >= 0) {
+        $(".counter").removeClass('error')
+    } else if (total < 0) {
+        $(".counter").addClass('error')
+    }
+    $(".counter span").text(total);
+  
+
+});
+
+
+});
