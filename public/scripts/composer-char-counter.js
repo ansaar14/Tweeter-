6b957dace23e124a9f97faf04170d067bfd9ae95
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-18 19:23:33 +0000

    cleaning up code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,23 +1,18 @@
-$(document).ready(function() {
-
-  $('textarea').keyup( function() {
+$(document).ready(function () {
+  $("textarea").keyup(function () {
     // console.log(this);
 
     let value = $(this).val();
     let maxlength = $(this).data("maxlength");
-    
+
     let total = maxlength - value.length;
     console.log(total);
 
     if (total >= 0) {
-        $(".counter").removeClass('error')
+      $(".counter").removeClass("error");
     } else if (total < 0) {
-        $(".counter").addClass('error')
+      $(".counter").addClass("error");
     }
     $(".counter span").text(total);
-  
-
-});
-
-
+  });
 });
