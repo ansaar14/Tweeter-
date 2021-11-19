cdb553ef52b07242ef55c5a8fc7cd7bb8f832199
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-19 02:03:09 +0000

    setting counter to 140 after tweet is sent

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,44 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// const $tweet = $(`<article class="tweet">Hello world</article>`);
-
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
-
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
 
 const renderTweets = function (tweets) {
   $(".tweets-container").empty();
@@ -127,6 +89,7 @@ $(document).ready(function () {
           loadTweets();
           $(".errormsg").hide();
           $("textarea").val("");
+          $(".counter").val(140);
         })
         .catch(function (error) {
           console.log(error.message);
